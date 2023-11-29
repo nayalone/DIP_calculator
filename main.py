3d31d75d34daccfb3185537e12f4d1f9eb796008
nayalone <hw45234@horwang.ac.th>
2023-11-29 11:02:50 +0700

    commit

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import tkinter as tk
 class mycalculator:
     def __init__(self):
 
-      self. root = tk.Tk()
+      self.root = tk.Tk()
 
       self.root.geometry ("300x300")
       self.root.title("mycalculator")
 
-      self.label = tk.Label(self.root, text-"hello world!", font=('Arial', 18)) 
+      self.label = tk.Label(self.root, text="hello world!", font=('Arial', 18)) 
       self.label. pack()
-      self.button = tk. Button(self.root, text-"Click Here!", height=4)
+      self.button = tk.Button(self.root, text="Click Here!", height=4)
       self.button.place(x=20, у=30)
 
-      self. root.mainloop()
+      self.root.mainloop()
 
 mycalculator ()
